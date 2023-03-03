900f218c50bd2b8f0749bc9ffd5c93a0eb1d976f
JeffersonPulido <jeffersonpulido55@gmail.com>
2023-03-03 11:55:29 -0500

    MongoDB Atlas Migracion

--- app/config/config.js
+++ app/config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 5000,
-    DB: process.env.DB || 'mongodb://127.0.0.1:27017/apiheippi'
+    // DB: process.env.DB || 'mongodb://127.0.0.1:27017/apiheippi'
+    DB: process.env.DB || 'mongodb+srv://jeffersonpulido55:Neneco041120@apiheippi.bijlkpk.mongodb.net/?retryWrites=true&w=majority'
 }
